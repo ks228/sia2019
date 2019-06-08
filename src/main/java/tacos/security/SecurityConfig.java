4687bd19518cec72bf3e51902991855fef04878c
khawar <ks228@st-andrews.ac.uk>
2019-06-08 06:03:59 +0100

    Chapter 4- JDBC-based user credential storage

--- src/main/java/tacos/security/SecurityConfig.java
+++ src/main/java/tacos/security/SecurityConfig.java
@@ -1,24 +1,29 @@
 package tacos.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.StandardPasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter{
 
+	@Autowired
+	javax.sql.DataSource dataSource;
+	
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth)
 	throws Exception {
-		auth.inMemoryAuthentication()
-			.withUser("buzz")
-				.password("{noop}infinity")
-				.authorities("ROLE_USER")
-			.and()
-			.withUser("woody")
-				.password("{noop}bullseye")
-				.authorities("ROLE_USER");
+		auth.jdbcAuthentication()
+			.dataSource(dataSource)
+			.usersByUsernameQuery(
+					"select username, password, enabled from Users where username=?")
+			.authoritiesByUsernameQuery(
+					"select username, authority from UserAuthorities where username=?")
+			.passwordEncoder(new BCryptPasswordEncoder());
 	}
 }
